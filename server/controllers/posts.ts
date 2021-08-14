049f448f387d3f5a026c7cd9c75dea3053640f2a
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-14 07:21:03 +0000

    make getPosts async

--- server/controllers/posts.ts
+++ server/controllers/posts.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
+import PostMessage from '../models/postMessage';
 
-export const getPosts = (req: Request, res: Response) => {
-    res.send('This works.');
+export const getPosts = async (req: Request, res: Response) => {
+    try {
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+
+    } catch (error) {
+        res.status(404).json( { message: error.message })
+    }
 }
+
+export const createPost = ((req: Request, res: Response) => {
+    res.send('Post Creation');
+})
