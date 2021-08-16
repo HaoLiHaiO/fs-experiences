27943a69d6c02fa2c1d1ef7446a5fb049909762b
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-16 12:10:05 +0000

    fix bug - use cors before creating routes

--- server/index.ts
+++ server/index.ts
@@ -6,11 +6,11 @@ import postRoutes from './routes/posts'
 
 const app = express();
 
-app.use('/posts', postRoutes)
-
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://alex:alex1301@cluster0.jbpst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
